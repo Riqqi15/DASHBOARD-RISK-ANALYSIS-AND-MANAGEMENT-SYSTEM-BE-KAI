8bc94784ae15ae92058597c7f9654b833fe44897
Riqqi15 <paimanyus2005@gmail.com>
2026-08-22 12:59:19 +0700

    Fix app page title for Sonar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>RAMS Dashboard</title>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <x-inertia::head />
